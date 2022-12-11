7ad2943d8443569e88069195cbbe793e937a3337
Gajanan Kabure <117345006+GK321@users.noreply.github.com>
2022-12-11 21:01:08 +0530

    Add files via upload
    Assignment Number:- 5
    Question Number :- 2

---
+++ _challenge2.py
@@ -0,0 +1,43 @@
+# Write a Python class called Calculator by completing the tasks below:
+# Implement an initializer to initialize the values of num1 and num2. Properties
+
+
+
+class cal():
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num1+self.num2
+    def mul(self):
+        return self.num1*self.num2
+    def div(self):
+        return self.num1/self.num2
+    def sub(self):
+        return self.num1-self.num2
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+obj=cal(num1,num2)
+choice=1
+while choice!=0:
+    print("0. Exit")
+    print("1. Add")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    choice=int(input("Enter choice: "))
+    if choice==1:
+        print("Result: ",obj.add())
+    elif choice==2:
+        print("Result: ",obj.sub())
+    elif choice==3:
+        print("Result: ",obj.mul())
+    elif choice==4:
+        print("Result: ",round(obj.div(),2))
+    elif choice==0:
+        print("Exiting!")
+    else:
+        print("Invalid choice!!")
+ 
+ 
+print()
